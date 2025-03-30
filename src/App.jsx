8375fa3e34b40677c8454a1d662ce32788311d4b
kunal jaiswal <kunaljaiswal262358@gmail.com>
2025-03-30 07:46:46 +0530

    Initial Commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,38 @@
-import { useState ,useEffect} from 'react'
+import { useState, useEffect } from 'react'
 import { stringify, v4 as uuidv4 } from 'uuid';
 import { CiEdit } from "react-icons/ci";
 import { MdDelete } from "react-icons/md";
-import Navbar from './components/Navbar'
 import './App.css'
+import './components/Navbar.jsx'
 
 function App() {
   const [todo, settodo] = useState("")
   const [todos, settodos] = useState([])
   const [temtodos, settemtodos] = useState([])
   const [showFinished, setshowFinished] = useState(true)
+  // const [time, setTime] = useState(new Date());
 
   useEffect(() => {
     let todos = localStorage.getItem("todos")
-    console.log(todos)
-    if(todos){
-      todos = JSON.parse(localStorage.getItem("todos")) 
-      console.log(todos)
+    if (todos) {
+      todos = JSON.parse(localStorage.getItem("todos"))
       settodos(todos)
     }
   }, [])
-  
 
-  const savetoLS = ()=>{
-    localStorage.setItem("todos" , JSON.stringify(todos))
+
+  const savetoLS = () => {
+    localStorage.setItem("todos", JSON.stringify(todos))
   }
 
   const handleChange = (e) => {
     settodo(e.target.value)
   }
 
-  const handleSave = (e , temtodos) => {
+  const handleSave = (e, temtodos) => {
     console.log(temtodos.length === 0)
-    if(temtodos.length === 0){
-      settodos([...todos , {id:uuidv4(), todo , isCompleted:false}])
+    if (temtodos.length === 0) {
+      settodos([...todos, { id: uuidv4(), todo, isCompleted: false }])
       console.log(todos)
       settodo("")
     } else {
@@ -47,31 +46,31 @@ function App() {
     savetoLS()
   }
 
-  const handleEdit = (e , id) => {
+  const handleEdit = (e, id) => {
     let newtodos = [...todos]
-    let index = todos.findIndex(item=>{
+    let index = todos.findIndex(item => {
       return item.id === id
     })
     settodo(newtodos[index].todo)
-    settemtodos([...temtodos , {newtodos , index:index}])
+    settemtodos([...temtodos, { newtodos, index: index }])
   }
 
-  const handleDelete = (e , id) => {
+  const handleDelete = (e, id) => {
     console.log(id)
-    const newtodos = todos.filter(item=>{
+    const newtodos = todos.filter(item => {
       return item.id !== id
     })
 
     settodos(newtodos)
     savetoLS()
   }
-  
+
   const handleCheckbox = (e, id) => {
     let newtodos = [...todos]
-    let index = todos.findIndex(item=>{
-      return item.id === id 
+    let index = todos.findIndex(item => {
+      return item.id === id
     })
-    newtodos[index].isCompleted  = !newtodos[index].isCompleted
+    newtodos[index].isCompleted = !newtodos[index].isCompleted
     settodos(newtodos)
     savetoLS()
   }
@@ -79,32 +78,34 @@ function App() {
   const toggleFinished = () => {
     setshowFinished(!showFinished)
   }
-  
-  
 
   return (
     <>
-      <Navbar />
-      <div className="md:container md:w-[35%] md:mx-auto min-h-[80vh] my-6 p-3 mx-5 bg-slate-200 rounded-xl">
+      
+      <div className="md:container md:w-[35%] md:mx-auto h-[80vh] my-6 p-3 mx-5 bg-gray-100 shadow-md rounded-xl">
         <h1 className='text-2xl text-center'>Write Here Your Todos</h1>
         <div className='flex justify-center items-center'>
           <input className='px-2 p-3  w-[80%] my-2 rounded-xl' type="text" onChange={handleChange} value={todo} />
-          <button disabled={todo.length<=3} className=" disabled:bg-violet-600 mx-5  my-2 p-1 px-3 bg-violet-900 text-white rounded-2xl" onClick={(e)=>{handleSave(e,temtodos)}} >Save</button>
+          <button disabled={todo.length <= 3} className=" disabled:bg-violet-600 mx-5  my-2 p-1 px-3 bg-violet-900 text-white rounded-2xl" onClick={(e) => { handleSave(e, temtodos) }} >Save</button>
         </div>
         <input type="checkbox" id='show' onChange={toggleFinished} checked={showFinished} />
-        <label className='mx-2' htmlFor="show">See Finished</label>
+        <label className='mx-2' htmlFor="show">Show Finished</label>
         <div className="divider h-[1px] bg-gray-500"></div>
         <div >
-          <div className="todos ">
-              {todos.length === 0 && <p>No todo is here</p>}
-              {todos.map(item=>{
-                return (showFinished || !item.isCompleted) && <div key={item.id} className="todo flex gap-1 my-3 items-center">
-                  <input id='item.id' checked={item.isCompleted} onChange={(e)=>{handleCheckbox(e ,item.id)}} type="checkbox" />
-                  <div className={item.isCompleted ? "line-through w-1/2" : "w-1/2"}>{item.todo}</div>
-                  <button className="edit mx-2  my-2 p-1 px-3 bg-violet-900 text-white rounded-2xl" onClick={(e)=>{handleEdit(e ,item.id)}} ><CiEdit /></button>
-                  <button className="delete mx-2  my-2 p-1 px-3 bg-violet-900 text-white  rounded-2xl" onClick={(e)=>{handleDelete(e ,item.id)}} ><MdDelete /></button>
+          <div className="todos h-[55vh] overflow-auto">
+            {todos.length === 0 && <p>No todo is here</p>}
+            {todos.map(item => {
+              return (showFinished || !item.isCompleted) && <div key={item.id} className="todo flex my-3 justify-between items-center">
+                <div className='content flex gap-2'>
+                  <input className='self-start mt-1' id='item.id' checked={item.isCompleted} onChange={(e) => { handleCheckbox(e, item.id) }} type="checkbox" />
+                  <div className={item.isCompleted ? "line-through " : ""}>{item.todo}</div>
                 </div>
-              })}
+                <div className='buttons flex gap-2'>
+                  <button className="edit float-end mx-2  my-2 p-1 px-3 bg-violet-900 text-white rounded-2xl" onClick={(e) => { handleEdit(e, item.id) }} ><CiEdit /></button>
+                  <button className="delete mx-2  my-2 p-1 px-3 bg-violet-900 text-white  rounded-2xl" onClick={(e) => { handleDelete(e, item.id) }} ><MdDelete /></button>
+                </div>
+              </div>
+            })}
           </div>
         </div>
       </div>
